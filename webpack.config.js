08441414fdf04f803a1b1c34045784f38b8ed12c
Lavinia Bianca <118028793+lavibia@users.noreply.github.com>
2025-01-13 17:59:32 +0200

    Basic configuration for the project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+ entry: './src/script.js',
+ devServer: {
+  static: './dist',
+ },
+ plugins: [
+  new HtmlWebpackPlugin({
+   title: 'DevFinder',
+   template: "src/index.html",
+  }),
+ ],
+ output: {
+  filename: 'index.js',
+  path: path.resolve(__dirname, 'dist'),
+ },
+ module: {
+  rules: [
+   {
+    test: /\.css$/i,
+    use: ['style-loader', 'css-loader'],
+   },
+   {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+   },
+  ],
+
+ },
+
+};
