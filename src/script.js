f2bc2e0f0bc0a1d4e2a5475ac8e7505a410fd171
Lavinia Bianca <118028793+lavibia@users.noreply.github.com>
2025-01-16 01:19:18 +0200

    Fix get user info

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 import "./style.css";
 import { Octokit } from "@octokit/core";
 
-
-
 console.log("Hello there");
 
 
@@ -19,8 +17,9 @@ function darkMode() {
   let element = document.body;
   element.classList.toggle("dark-mode");
 }
+getGithubUser();
 
-getGithubUser()
+
 async function getGithubUser() {
   const octokit = new Octokit({
     auth: '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'
@@ -32,18 +31,46 @@ async function getGithubUser() {
       'X-GitHub-Api-Version': '2022-11-28'
     }
   }).then(res => {
-    console.log(res.data);
+
     if (res.status == 200) {
-      displayUserData(res.data);
+      displayUserData(res.data)
     }
 
   }).catch(err => {
+    displayNotFound();
     console.log(err);
     //Style user not found in the search box
   })
 }
 
-function displayUserData(user) {
+function displayNotFound() {
+  console.log('not found')
+}
+// async function getGithubUser() {
+//   const octokit = new Octokit({
+//     auth: `${process.env.API_KEY}`
+//   });
+
+//   await octokit.request('GET /users/{username}', {
+//     username: 'octocat',
+//     headers: {
+//       'X-GitHub-Api-Version': '2022-11-28'
+//     }
+//   }).then(res => {
+//     console.log(res.data);
+//     if (res.status == 200) {
+//       displayUserData(res.data);
+//     }
+
+//   }).catch(err => {
+//     console.log(err);
+//     //Style user not found in the search box
+//   })
+// }
+
+async function displayUserData(user) {
+  console.log(user)
+  console.log(await user.blog);
   // displaying data
 
   // let bio = document.createElement('p')
